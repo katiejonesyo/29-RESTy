2ed73f4b027c828c6443bd4e4a2e3691db0e5307
Katelinn Jones <katiejonesyo@gmail.com>
2021-02-06 18:18:11 -0800

    going to scrap everything and follow along with something else

---
+++ src/components/displays/Response.jsx
@@ -0,0 +1,19 @@
+
+import React from 'react';
+import propTypes from 'prop-types';
+import ReactJson from 'react-json-view'
+
+const Response = ({ apiResponse }) => (
+  <section>
+    <ReactJson src={apiResponse} />
+  </section>
+);
+
+Response.propType = {
+  apiResponse: propTypes.object.isRequired
+};
+
+export default Response;
+
+
+
--- src/containers/Resty.jsx
+++ src/containers/Resty.jsx
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
+import { getRequest } from '../services/api';
 import List from '../components/displays/List';
 import Controls from '../components/controls/Controls';
+import Response from '../components/displays/Response';
+
 
 
 export default class Resty extends Component {
@@ -8,7 +11,6 @@ export default class Resty extends Component {
     state = {
         url: '',
         method: '',
-        json: 'sample jason response',
         response: '',
         history: [],
         click: false
@@ -19,23 +21,33 @@ export default class Resty extends Component {
  this.setState({ [e.target.name]: e.target.value })
     }
 
-    handleClick = (e) => {
+    handleClick = async() => {
        if (!this.state.url || !this.state.method)
         return alert('Fill out required fields.')
 
         this.setState(state =>({ 
             history: [...state.history, {url: state.url, method:state.method }],
             click:true }))
+
+        if (this.state.method === 'GET'){
+            const response = await getRequest(this.state.url)
+            this.setState({ response: respose })
+        }
     }
 
     render() {
-        const { click, history } = this.state;
+        const { click, history, response } = this.state;
         return (
             <>
             <h1> Resty </h1>
             <div> Resty 
                 <Controls onChange={this.handleChange} onClick={this.onClick} />
-                {click && <List history={history} />}
+                { click && 
+                  <>
+                  <List history={history} />
+                  <Response apiResponse={response}/>
+                  </>
+                }
             </div>
             </>
         )
